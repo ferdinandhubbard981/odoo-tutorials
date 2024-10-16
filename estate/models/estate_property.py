9ff57866611cebd96f6aca5de14fb982ef36b8a3
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2024-10-16 15:09:46 +0100

    completed chapter 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 
 class Property(models.Model):
@@ -7,13 +8,18 @@ class Property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: self._in_three_months())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([("north", "North"), ("east", "East"), ("south", "South"), ("west", "West")])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")], default="new")
+
+    def _in_three_months(self):
+        return fields.Date.today() + relativedelta(months=3)
