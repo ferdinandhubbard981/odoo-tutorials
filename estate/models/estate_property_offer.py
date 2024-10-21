e7e1c7cb0addf88f12aa18582c0f2aea61ca8cd3
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2024-10-21 11:37:03 +0100

    completed inverse section of chapter 8

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
+
 
 class offer(models.Model):
     _name = "estate.property.offer"
@@ -6,3 +8,14 @@ class offer(models.Model):
     status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")], name="Status", copy=False)
     partner_id = fields.Many2one("res.partner", name="Buyer", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline", readonly=False)
+    validity = fields.Integer(string="Validity (Days)", default=7, readonly=False)
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.today() + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = record.date_deadline.toordinal() - fields.Date.today().toordinal()
